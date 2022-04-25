38db358e845bf3dc00ea47542cafc59f9f65a8af
SteveMcNeill <smcneill@swva.net>
2022-04-25 09:24:10 -0400

    Fix to compile issue
    revert to old internal structure of source for now.

--- internal/source/recompile_win.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd %0\..\
-echo Recompiling...
-cd ../c
-c_compiler\bin\g++ -mconsole -s -Wfatal-errors -w -Wall qbx.cpp  libqb\os\win\libqb_2_1_1000000101000.o  -D DEPENDENCY_SOCKETS -D DEPENDENCY_NO_PRINTER -D DEPENDENCY_ICON -D DEPENDENCY_NO_SCREENIMAGE -D DEPENDENCY_LOADFONT  parts\video\font\ttf\os\win\src.o   parts\core\os\win\src.a -lopengl32 -lglu32   -mwindows -static-libgcc -static-libstdc++ -D GLEW_STATIC -D FREEGLUT_STATIC     -lws2_32 -lwinmm -lgdi32 -o "..\..\qb64(2).exe"
-pause
