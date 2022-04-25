e83304b33c9201aeaa1a7f78517513a7ffa934d4
Matthew Kilgore <mattkilgore12@gmail.com>
2022-04-25 01:33:39 -0400

    Switch to matrix-based build

--- .ci/deploy.ps1
+++
@@ -1,7 +0,0 @@
-
-$Filename = "qb64_win-${Env:PLATFORM}.7z"
-
-Set-Location ..
-7z a "-xr@$QB64pe\.ci\common-exclusion.list" "-xr@$QB64pe\.ci\win-exclusion.list" $Filename "QB64pe"
-
-Move-Item -Path $Filename -Destination ./QB64pe
