5f3703cffdc8a93179db6d00f817f496660853a8
Matthew Kilgore <mattkilgore12@gmail.com>
2022-04-25 01:33:39 -0400

    Switch to matrix-based build

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-OS=$1
+buildPlatform=$1
 
-filename="qb64_${OS}.tar.gz"
+case "$buildPlatform" in
+    windows-latest)
+        filename="qb64_win-$PLATFORM.7z"
 
-cd ..
-tar --create --auto-compress --file ${filename} --exclude-from=./QB64pe/.ci/common-exclusion.list --exclude-from=./QB64pe/.ci/$OS-exclusion.list ./QB64pe
+        cd ..
+        7z a "-xr@QB64pe\.ci\common-exclusion.list" "-xr@QB64pe\.ci\win-exclusion.list" "$filename" "QB64pe"
+        ;;
+
+    ubuntu-latest)
+        filename="qb64_lnx.tar.gz"
+
+        cd ..
+        tar --create --auto-compress --file ${filename} --exclude-from=./QB64pe/.ci/common-exclusion.list --exclude-from=./QB64pe/.ci/lnx-exclusion.list ./QB64pe
+        ;;
+
+    macos-latest)
+        filename="qb64_osx.tar.gz"
+
+        cd ..
+        tar --create --auto-compress --file ${filename} --exclude-from=./QB64pe/.ci/common-exclusion.list --exclude-from=./QB64pe/.ci/osx-exclusion.list ./QB64pe
+        ;;
+
+esac
+
 mv "./$filename" "./QB64pe/"
