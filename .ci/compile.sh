707480642b2af274fcde075eacc03c78461cf23f
Matthew Kilgore <mattkilgore12@gmail.com>
2022-04-27 18:55:16 -0400

    Update ./internal/source automatically

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -2,10 +2,14 @@
 
 ./qb64_bootstrap -x -w source/qb64.bas
 SUCCESS=$?
+
 rm qb64_bootstrap
 rm internal/source/*
+rm internal/temp/debug_* internal/temp/recompile_*
+
 mv internal/temp/* internal/source/
-rm internal/source/debug_* internal/source/recompile_*
+
 find . -type f -iname "*.a" -exec rm {} \;
 find . -type f -iname "*.o" -exec rm {} \;
+
 exit $SUCCESS
