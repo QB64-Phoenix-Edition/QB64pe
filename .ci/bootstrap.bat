7d7d16c4f698440beeb0757425347007e0a9c5bc
Matthew Kilgore <mattkilgore12@gmail.com>
2022-05-06 17:43:27 -0400

    Use $EXEICON and $VERSIONINFO in qb64.bas

--- .ci/bootstrap.bat
+++ .ci/bootstrap.bat
@@ -38,8 +38,6 @@ cd ..\..\..\..\..\..
 
 echo Bootstrapping QB64
 copy internal\source\*.* internal\temp\ >nul
-copy source\qb64.ico internal\temp\ >nul
-copy source\icon.rc internal\temp\ >nul
 cd internal\c
 c_compiler\bin\windres.exe -i ..\temp\icon.rc -o ..\temp\icon.o
 c_compiler\bin\g++ -mconsole -s -Wfatal-errors -w -Wall qbx.cpp libqb\os\win\libqb_setup.o ..\temp\icon.o -D DEPENDENCY_LOADFONT  parts\video\font\ttf\os\win\src.o -D DEPENDENCY_SOCKETS -D DEPENDENCY_NO_PRINTER -D DEPENDENCY_ICON -D DEPENDENCY_NO_SCREENIMAGE parts\core\os\win\src.a -lopengl32 -lglu32   -mwindows -static-libgcc -static-libstdc++ -D GLEW_STATIC -D FREEGLUT_STATIC     -lws2_32 -lwinmm -lgdi32 -o "..\..\qb64_bootstrap.exe"
