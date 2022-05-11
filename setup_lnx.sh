ec484f5b1e1ee832331ee39f7c4456c0021eca26
Matthew Kilgore <mattkilgore12@gmail.com>
2022-05-10 23:04:00 -0400

    Add make to Linux dependency list

--- setup_lnx.sh
+++ setup_lnx.sh
@@ -75,25 +75,25 @@ fi
 #Find and install packages
 if [ "$DISTRO" == "arch" ]; then
   echo "ArchLinux detected."
-  pkg_list="gcc zlib xorg-xmessage $GET_WGET"
+  pkg_list="gcc make zlib xorg-xmessage $GET_WGET"
   installed_packages=`pacman -Q`
   installer_command="sudo pacman -S "
   pkg_install
 elif [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "zorin" ]; then
   echo "Debian based distro detected."
-  pkg_list="g++ x11-utils mesa-common-dev libglu1-mesa-dev libasound2-dev zlib1g-dev $GET_WGET"
+  pkg_list="g++ make x11-utils mesa-common-dev libglu1-mesa-dev libasound2-dev zlib1g-dev $GET_WGET"
   installed_packages=`dpkg -l`
   installer_command="sudo apt-get -y install "
   pkg_install
 elif [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "redhat" ] || [ "$DISTRO" == "centos" ]; then
   echo "Fedora/Redhat based distro detected."
-  pkg_list="gcc-c++ xmessage mesa-libGLU-devel alsa-lib-devel zlib-devel $GET_WGET"
+  pkg_list="gcc-c++ make xmessage mesa-libGLU-devel alsa-lib-devel zlib-devel $GET_WGET"
   installed_packages=`yum list installed`
   installer_command="sudo yum install "
   pkg_install
 elif [ "$DISTRO" == "voidlinux" ]; then
    echo "VoidLinux detected."
-   pkg_list="gcc xmessage glu-devel zlib-devel alsa-lib-devel $GET_WGET"
+   pkg_list="gcc make xmessage glu-devel zlib-devel alsa-lib-devel $GET_WGET"
    installed_packages=`xbps-query -l |grep -v libgcc`
    installer_command="sudo xbps-install -Sy "
    pkg_install
