e097a1942a7740fdf24600f64b34fa5a83e6c5db
Matthew Kilgore <mattkilgore12@gmail.com>
2022-05-14 15:34:39 -0400

    Remvoe unnecessary cd

--- .ci/compile.bat
+++ .ci/compile.bat
@@ -4,8 +4,6 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 qb64_bootstrap.exe -x -w source\qb64.bas
 IF ERRORLEVEL 1 exit /b 1
 
-cd ..\..
-
 del qb64_bootstrap.exe
 del /q /s internal\source\*
 move internal\temp\* internal\source\
