e191abfdad74cb05e010a3a427fd92a47edea7d0
Matthew Kilgore <mattkilgore12@gmail.com>
2022-05-14 15:00:32 -0400

    Update ./internal/source when building on Windows
    On Windows we don't distribute the ./setup_win.bat, so the contents of
    ./internal/source are not actually relevant for them. Since the QB64
    symbol file will be located in it though, it is important that they get
    an updated copy. It does also just make basic sense that if we're
    distributing an ./internal/source with the Windows version it should
    match whatever QB64 it was shipped with.

--- .ci/compile.bat
+++ .ci/compile.bat
@@ -6,6 +6,8 @@ IF ERRORLEVEL 1 exit /b 1
 
 cd ..\..
 
-internal\c\c_compiler\bin\mingw32-make.exe OS=win clean
-
 del qb64_bootstrap.exe
+del /q /s internal\source\*
+move internal\temp\* internal\source\
+
+internal\c\c_compiler\bin\mingw32-make.exe OS=win clean
