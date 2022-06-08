014d0296f5182d3bc81c63be2a29a80bf75d9ace
Matthew Kilgore <mattkilgore12@gmail.com>
2022-06-07 22:37:30 -0400

    Use -j8, pass OS to ./.ci/compile.sh

--- .ci/bootstrap.sh
+++ .ci/bootstrap.sh
@@ -3,7 +3,7 @@
 OS=$1
 
 echo -n "Bootstrapping QB64..."
-make OS=$OS BUILD_QB64=y EXE=./qb64_bootstrap
+make -j8 OS=$OS BUILD_QB64=y EXE=./qb64_bootstrap
 
 if [ $? -ne 0 ]; then
   echo "QB64 bootstrap failed"
