c0e3f3608c508078158cd6347d09c1eec5db2f13
Samuel Gomes <47574584+a740g@users.noreply.github.com>
2022-09-18 23:46:54 +0530

    Add O2 to build.mk

--- internal/c/parts/video/image/build.mk
+++ internal/c/parts/video/image/build.mk
@@ -5,7 +5,7 @@ IMAGE_SRCS := \
 IMAGE_OBJS := $(patsubst %.cpp,$(PATH_INTERNAL_C)/parts/video/image/%.o,$(IMAGE_SRCS))
 
 $(PATH_INTERNAL_C)/parts/video/image/%.o: $(PATH_INTERNAL_C)/parts/video/image/%.cpp
-	$(CXX) $(CXXFLAGS) -Wall -DDEPENDENCY_CONSOLE_ONLY $< -c -o $@
+	$(CXX) -O2 $(CXXFLAGS) -DDEPENDENCY_CONSOLE_ONLY -Wall $< -c -o $@
 
 ifdef DEP_IMAGE_CODEC
     EXE_LIBS += $(IMAGE_OBJS)
