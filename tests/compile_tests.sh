d8a3ec75fd4ae639a9a936ef6ffb12ae63036a9e
Matthew Kilgore <mattkilgore12@gmail.com>
2022-10-07 21:18:25 -0400

    Run tests with -O2
    Since we do allow users to use optimization, it's worth running our
    tests with it to verify at least those work.

--- tests/compile_tests.sh
+++ tests/compile_tests.sh
@@ -76,7 +76,7 @@ do
     cd "./tests/compile_tests/$category"
 
     # -m and -q make sure that we get predictable results
-    "../../../$QB64" $compilerFlags -q -m -x "$testName.bas" -o "../../../$EXE" 1>"../../../$compileResultOutput"
+    "../../../$QB64" "-f:OptimizeCppProgram=true" $compilerFlags -q -m -x "$testName.bas" -o "../../../$EXE" 1>"../../../$compileResultOutput"
     ERR=$?
 
     popd >/dev/null
