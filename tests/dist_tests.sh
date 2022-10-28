QB64-Phoenix-Edition/QB64pe

commit 5031de2cd6458b1db4a265a4978408dbf35284fa

Add ability to avoid starting QB64 after script is run

setup_osx.command
@@ -1,4 +1,9 @@
-cd "$(dirname "$0")"
+# Argument 1: Location of qb64 (blank means current directory
+# Argument 2: If not blank, qb64 will not be started after compiltaion
+
+cd "$(dirname "$1")"
+dont_run="$2"
+
 Pause()
 {
 OLDCONFIG=`stty -g`
@@ -28,7 +33,7 @@ if [ -z "$(which clang++)" ]; then
   echo "Attempting to install Apple's Command Line Tools for Xcode..."
   echo "After installation is finished, run this setup script again."
   xcode-select --install
-  Pause
+  [ -z "$dont_run" ] && Pause
   exit 1
 fi
 
@@ -38,7 +43,7 @@ rm -f libqb_setup.o
 ./setup_build.command
 if [ ! -f ./libqb_setup.o ]; then
   echo "Compilation of ./internal/c/libqb/os/osx/libqb_setup.o failed!"
-  Pause
+  [ -z "$dont_run" ] && Pause
   exit 1
 fi
 popd >/dev/null
@@ -49,7 +54,7 @@ rm -f src.o
 ./setup_build.command
 if [ ! -f ./src.o ]; then
   echo "Compilation of ./internal/c/parts/video/font/ttf/os/osx/src.o failed!"
-  Pause
+  [ -z "$dont_run" ] && Pause
   exit 1
 fi
 popd >/dev/null
@@ -62,14 +67,17 @@ popd >/dev/null
 
 echo ""
 if [ -f ./qb64 ]; then
-  echo "Launching 'QB64'"
-  ./qb64
+  if [ -z "$dont_run" ]; then
+    echo "Launching 'QB64'"
+    ./qb64
+  fi
   echo ""
   echo "Note: 'qb64' is located in same folder as this setup program."
   echo "Press any key to continue..."
-  Pause
+  [ -z "$dont_run" ] && Pause
+  exit 0
 else
   echo "Compilation of QB64 failed!"
-  Pause
+  [ -z "$dont_run" ] && Pause
   exit 1
 fi

tests/dist_tests.sh
@@ -22,12 +22,12 @@ case "$2" in
         ;;
 
     lnx)
-        ./setup_lnx.sh 1>"$RESULTS_DIR/linux-setup.txt"
+        ./setup_lnx.sh "dont_run" 1>"$RESULTS_DIR/linux-setup.txt"
         assert_success_named "Linux setup" cat "$RESULTS_DIR/linux-setup.txt"
         ;;
 
     osx)
-        ./setup_osx.command 1>"$RESULTS_DIR/osx-setup.txt"
+        ./setup_osx.command "." "dont_run" 1>"$RESULTS_DIR/osx-setup.txt"
         assert_success_named "OSX setup" cat "$RESULTS_DIR/osx-setup.txt"
         ;;
 esac

setup_lnx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#QB64 Installer -- Shell Script -- Matt Kilgore 2013
-#Version 5 -- January 2020
+# QB64 Installer
+# Argument 1: If not blank, qb64 will not be started after compilation
+
+dont_run="$1"
 
 #This checks the currently installed packages for the one's QB64 needs
 #And runs the package manager to install them if that is the case
@@ -164,8 +166,11 @@ Path=$_pwd
 StartupNotify=false
 EOF
 
-  echo "Running QB64..."
-  ./qb64 &
+  if [ -z "$dont_run" ]; then
+    echo "Running QB64..."
+    ./qb64 &
+  fi
+
   echo "QB64 is located in this folder:"
   echo "`pwd`"
   echo "There is a ./run_qb64.sh script in this folder that should let you run qb64 if using the executable directly isn't working."

commit 37f2ecd4ce004da6a43ff99951c7a6b878d3e8d1

Fix setup_osx.command script when run from a different directory
I broke this by accident in 5031de2, likely by misreading the $0.  What
the line was intended to do was change to the directory the script is
located in when the script starts, this allows the script to be started
from any location.

Typically this isn't necessary functionality, but for Mac OS it's
important because if you double-click the script (the typical way to run
it) it is started from the user's home directory. Due to this I also
updated the Mac OS distribution test to run the script in this way so
that it's closer to how a user will run it.

Fixes: #218

setup_osx.command
@@ -1,8 +1,7 @@
-# Argument 1: Location of qb64pe (blank means current directory
-# Argument 2: If not blank, qb64pe will not be started after compiltaion
+# Argument 1: If not blank, qb64pe will not be started after compiltaion
 
-cd "$(dirname "$1")"
-dont_run="$2"
+cd "$(dirname "$0")"
+dont_run="$1"
 
 Pause()
 {

tests/dist_tests.sh
@@ -35,7 +35,17 @@ case "$2" in
         ;;
 
     osx)
-        ./setup_osx.command "." "dont_run" 1>"$RESULTS_DIR/osx-setup.txt"
+        # When testing the OSX script we run it from a different directory as
+        # that is the typical way it is used.
+        pushd . > /dev/null
+        cd "$ROOT"
+
+        $1/setup_osx.command "dont_run" 1>"$RESULTS_DIR/osx-setup.txt"
+        ERR=$?
+
+        popd > /dev/null
+
+        (exit $ERR)
         assert_success_named "OSX setup" cat "$RESULTS_DIR/osx-setup.txt"
         ;;
 esac