3666f0763a656022c54cccd1ca1503edb6b253f4
a740g <samuel_gomes@outlook.com>
2023-10-09 13:49:05 +0530

    Avoid miniaudio runtime linking on macOS

--- internal/c/parts/audio/miniaudio_impl.cpp
+++ internal/c/parts/audio/miniaudio_impl.cpp
@@ -12,6 +12,11 @@
 // Enable Ogg Vorbis decoding
 #define STB_VORBIS_HEADER_ONLY
 #include "extras/stb_vorbis.c"
+// Due to the way miniaudio links to macOS frameworks at runtime, the application may not pass Apple's notarization process :(
+// So, we will avoid runtime linking on macOS. See this discussion for more info: https://github.com/mackron/miniaudio/issues/203
+#ifdef __APPLE__
+#    define MA_NO_RUNTIME_LINKING
+#endif
 // The main miniaudio header
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
