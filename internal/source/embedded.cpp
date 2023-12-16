eea115aa7b3bd04b77343c077b95b19065e35231
Roland Heyder <support@rhosigma-cw.net>
2023-12-16 13:17:29 +0100

    Fixing broken internal/source

---
+++ internal/source/embedded.cpp
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <string.h>
+#include "../c/libqb.h"
+#include "../c/libqb/include/compression.h"
+
+qbs *func__embedded(qbs *handle)
+{
+    return qbs_new_txt("");
+}
+
