0dc26ee0892c43314f1baf720d44af86f94f63bd
Roland Heyder <support@rhosigma-cw.net>
2025-10-22 22:34:07 +0200

    Fix networking tests
    - taken from Samuel's QB64pe fork
    - sorry Sam, was hoping you complete your PR last weekend so I could have build my PR on that

--- tests/c/http.cpp
+++ tests/c/http.cpp
@@ -4,70 +4,19 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "test.h"
 #include "http.h"
+#include "test.h"
 
-const char *example_result =
-"<!doctype html>\n"
-"<html>\n"
-"<head>\n"
-"    <title>Example Domain</title>\n"
-"\n"
-"    <meta charset=\"utf-8\" />\n"
-"    <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />\n"
-"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n"
-"    <style type=\"text/css\">\n"
-"    body {\n"
-"        background-color: #f0f0f2;\n"
-"        margin: 0;\n"
-"        padding: 0;\n"
-"        font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;\n"
-"        \n"
-"    }\n"
-"    div {\n"
-"        width: 600px;\n"
-"        margin: 5em auto;\n"
-"        padding: 2em;\n"
-"        background-color: #fdfdff;\n"
-"        border-radius: 0.5em;\n"
-"        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n"
-"    }\n"
-"    a:link, a:visited {\n"
-"        color: #38488f;\n"
-"        text-decoration: none;\n"
-"    }\n"
-"    @media (max-width: 700px) {\n"
-"        div {\n"
-"            margin: 0 auto;\n"
-"            width: auto;\n"
-"        }\n"
-"    }\n"
-"    </style>    \n"
-"</head>\n"
-"\n"
-"<body>\n"
-"<div>\n"
-"    <h1>Example Domain</h1>\n"
-"    <p>This domain is for use in illustrative examples in documents. You may use this\n"
-"    domain in literature without prior coordination or asking for permission.</p>\n"
-"    <p><a href=\"https://www.iana.org/domains/example\">More information...</a></p>\n"
-"</div>\n"
-"</body>\n"
-"</html>\n"
-;
+const char *example_result = "<!doctype html><html lang=\"en\"><head><title>Example Domain</title><meta name=\"viewport\" content=\"width=device-width, "
+                             "initial-scale=1\"><style>body{background:#eee;width:60vw;margin:15vh "
+                             "auto;font-family:system-ui,sans-serif}h1{font-size:1.5em}div{opacity:0.8}a:link,a:visited{color:#348}</"
+                             "style><body><div><h1>Example Domain</h1><p>This domain is for use in documentation examples without needing permission. Avoid "
+                             "use in operations.<p><a href=\"https://iana.org/domains/example\">Learn more</a></div></body></html>\n";
 
 void test_http() {
     size_t expected_result_len = strlen(example_result);
-    const char *urls[] = {
-        "http://www.example.com",
-        "https://www.example.com",
-        "HTTPS://WWW.EXAMPLE.COM",
-        "httP://wwW.example.com",
-        "www.example.com",
-        "http://www.example.com:80",
-        "https://www.example.com:443",
-        NULL
-    };
+    const char *urls[] = {"http://www.example.com", "https://www.example.com",   "HTTPS://WWW.EXAMPLE.COM",     "httP://wwW.example.com",
+                          "www.example.com",        "http://www.example.com:80", "https://www.example.com:443", NULL};
 
     for (const char **url = urls; *url; url++) {
         int c = libqb_http_open(*url, 1);
@@ -107,7 +56,7 @@ int main() {
 
     int ret;
     struct unit_test tests[] = {
-        { test_http, "http" },
+        {test_http, "http"},
     };
 
     ret = run_tests("http", tests, sizeof(tests) / sizeof(*tests));
