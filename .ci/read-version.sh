212603dd6fb686b86bc46f583575dd7a628821b5
Matthew Kilgore <mattkilgore12@gmail.com>
2026-08-31 21:00:52 -0400

    Fix version detection script

--- .ci/read-version.sh
+++ .ci/read-version.sh
@@ -4,7 +4,7 @@
 versionLine=$(cat ./source/global/version.bas | grep "Version\$ = " | head -n 1 | tr -d '\n' | tr -d '\r')
 
 # This uses a regular expression to pull the X.Y.Z version out of the assignment line
-version=$(echo "$versionLine" | sed -r 's/^Version\$ = "([0-9]*\.[0-9]*\.[0-9]*)"/\1/' | tr -d '\n')
+version=$(echo "$versionLine" | sed -r 's/^Version\$ = "([0-9]*\.[0-9]*\.[0-9]*.*)"/\1/' | tr -d '\n')
 
 # if ./internal/version.txt exists, then we have a tag at the end of our version number
 # (IE. non-release version)
